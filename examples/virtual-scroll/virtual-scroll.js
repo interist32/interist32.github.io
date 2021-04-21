20a9f80de7352396e6138d8bec458b5f7017d569
interist32-mi\interist32 <interist32@gmail.com>
2021-04-21 12:29:31 -0700

    2021-04-21 12:29:24

---
+++ examples/virtual-scroll/virtual-scroll.js
@@ -0,0 +1,19 @@
+class VirtualScroll {
+  constructor({
+    container,
+    data,
+    containerHeightPx,
+    rowHeightPx,
+    renderRow,
+  }) {
+    this.container = container;
+    this.items = items;
+    this.containerHeightPx = containerHeightPx;
+    this.rowHeightPx = rowHeightPx;
+    this.renderRow = renderRow;
+
+    this.render();
+  }
+
+  render() {}
+}
